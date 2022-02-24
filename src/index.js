951618f5b0cd94ab751d83ebca9f6e64dc26a452
QuentinBaillou <quentin.baillou@hotmail.fr>
2022-02-24 13:31:23 +0100

    Fix - Change App folder in Otravel/ and import in index.js modified

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import { Provider } from 'react-redux';
 import store from 'src/store';
 
 // Composants
-import App from 'src/components/App';
+import Otravel from 'src/components/Otravel';
 
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
   <Provider store={store}>
-    <App />
+    <Otravel />
   </Provider>
 );
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
