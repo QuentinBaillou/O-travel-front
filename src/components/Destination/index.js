eaeaacafcbef77653f77f1c1ef544376f9bd7d2c
OlivierThuymans <olivier.thuymans@hotmail.com>
2022-02-28 10:10:09 +0100

    Func : creation of Destination page

---
+++ src/components/Destination/index.js
@@ -0,0 +1,12 @@
+import './style.scss';
+
+const Destination = () => {
+
+return (
+  <div className="container">
+    Bloc principal
+  </div>
+);
+};
+
+export default Destination;
--- src/components/Otravel/index.js
+++ src/components/Otravel/index.js
@@ -1,10 +1,12 @@
 // == Import
 import Header from 'src/components/Header';
+import Destination from 'src/components/Destination';
 
 // == Composant
 const Otravel = () => (
   <div className="otravel">
     <Header />
+    <Destination />
   </div>
 );
 
