1742b499fb6c179251f583a4d943401ec59c01f0
OlivierThuymans <olivier.thuymans@hotmail.com>
2022-02-28 15:34:23 +0100

    Format: Summary component finished

--- src/components/Destination/Summary.js
+++ src/components/Destination/Summary.js
@@ -2,7 +2,23 @@ import './style.scss';
 
 const Summary = () => {
   return (
-  <div>composant Summary</div>
+  <div className="summary_wrapper">
+    <div className="summary">
+      Soluta voluptas a accusamus, eius voluptatibus iusto molestiae, vitae cupiditate necessitatibus facilis minima fugiat placeat. Ab mollitia, in totam consectetur iste esse quos reprehenderit quod iusto soluta nulla. Cupiditate, nostrum. 
+    </div>
+
+    <div className="strong_points">
+
+      <span>Points forts</span>
+      <br /><br />
+      <ul>
+        <span className="bullet_point">&#8227;</span><li> Soluta voluptas a accusamus</li><br />
+        <span className="bullet_point">&#8227;</span><li> Ab mollitia, in totam consectetur iste esse</li><br />
+        <span className="bullet_point">&#8227;</span><li> Quos reprehenderit quod iusto soluta nulla.</li><br />
+      </ul>
+
+    </div>
+  </div>
   );
 };
 
