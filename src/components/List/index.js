53253c1058e3b8955f5cba766fe6732a252d306c
Rudylefrancois <rudylefrancois18@gmail.com>
2022-03-01 15:24:05 +0100

    delete card composant list

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,3 +1,4 @@
+// == Import
 import './style.scss';
 
 import train from 'src/assets/images/train-1542096_1920.jpg';
@@ -5,61 +6,11 @@ import flag from 'src/assets/images/canada-svgrepo-com.svg';
 
 import { Card, Image, Pagination } from 'semantic-ui-react';
 
+// == Composant
 const List = () => (
   <div className="trip-list">
     <h2 className="trip-title">Les voyages</h2>
     <Card.Group>
-      <Card>
-        <Card.Content>
-          <Image src={flag} size="mini" />
-          <Image
-            src={train}
-            ui={false}
-            fluid
-            label={{ as: 'a', corner: 'right', icon: 'star' }}
-          />
-          <Card.Header>
-            Rocky Mountaineer
-            <Card.Description>
-              8 nuits | train
-            </Card.Description>
-          </Card.Header>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Image src={flag} size="mini" />
-          <Image
-            src={train}
-            ui={false}
-            fluid
-            label={{ as: 'a', corner: 'right', icon: 'star' }}
-          />
-          <Card.Header>
-            Rocky Mountaineer
-            <Card.Description>
-              8 nuits | train
-            </Card.Description>
-          </Card.Header>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Image src={flag} size="mini" />
-          <Image
-            src={train}
-            ui={false}
-            fluid
-            label={{ as: 'a', corner: 'right', icon: 'star' }}
-          />
-          <Card.Header>
-            Rocky Mountaineer
-            <Card.Description>
-              8 nuits | train
-            </Card.Description>
-          </Card.Header>
-        </Card.Content>
-      </Card>
       <Card>
         <Card.Content>
           <Image src={flag} size="mini" />
@@ -123,4 +74,5 @@ const List = () => (
   </div>
 );
 
+// == Export
 export default List;
