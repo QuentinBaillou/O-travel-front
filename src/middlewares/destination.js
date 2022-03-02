2c7efd2d1c96b3b510698c3b901f2620894290d6
QuentinBaillou <quentin.baillou@hotmail.fr>
2022-03-02 13:22:52 +0100

    Build done

--- src/middlewares/destination.js
+++ src/middlewares/destination.js
@@ -4,19 +4,19 @@ import { GET_DESTINATIONS } from 'src/actions/destinationActions';
 const fetchDestinations = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_DESTINATIONS:
-      axios.get(`http://cedric-vandermaes.vpnuser.lan:8080/api/`)
-         .then((response) => {
-           console.log(response.data);
-           //store.dispatch();
-         })
-         .catch((error) => {
-           console.log('erreur : ', error);
-         });
-       next(action);
-       break;
+      axios.get('http://cedric-vandermaes.vpnuser.lan:8080/api/')
+        .then((response) => {
+          console.log(response.data);
+          // store.dispatch();
+        })
+        .catch((error) => {
+          console.log('erreur : ', error);
+        });
+      next(action);
+      break;
 
-       default:
-       next(action);
+    default:
+      next(action);
   }
 };
 
