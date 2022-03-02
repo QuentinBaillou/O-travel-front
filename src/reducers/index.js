537e034f3021f4b69420653821b49651c252fdd5
OlivierThuymans <olivier.thuymans@hotmail.com>
2022-03-02 10:25:37 +0100

    Fix: fixing bug. Reducer missing in rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import destinationReducer from './destination';
 
 const rootReducer = combineReducers({
-
+  destination: destinationReducer,
 });
 
 export default rootReducer;
