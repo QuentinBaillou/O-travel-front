1f34c30559a42fd435fb678e87121c55560b542c
Rudylefrancois <rudylefrancois18@gmail.com>
2022-03-04 14:51:48 +0100

    func : add style component profil

--- src/components/Profil/index.js
+++ src/components/Profil/index.js
@@ -3,45 +3,53 @@ import './style.scss';
 
 import { useDispatch } from 'react-redux';
 
-import { Container, Image, List, Button } from 'semantic-ui-react';
+import { Image, List, Button } from 'semantic-ui-react';
 
 // == Composant
 const Profil = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className='profil'>
-      <h2>Mon profil</h2>
-      <Image
-        src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-        size="medium"
-        circular
-      />
-      <List>
-        <List.Item>
-          <List.Icon name='user' />
-          <List.Content>John | Wilkinson  </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Icon name='marker' />
-          <List.Content>New York, NY</List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Icon name='mail' />
-          <List.Content>
-            <a href='mailto:Yves@travelInc.com'>John@travelInc.com</a>
-          </List.Content>
-        </List.Item>
-      </List>
-      <Button>Créer/Modifier</Button>
-      <Button>Changer mot de passe</Button>
-      <div className='favorites'>
-        <h2>Listes favoris</h2>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' 
-        size='medium' disabled 
+    <div className="profil">
+      <h2 className="profil-title">Mon profil</h2>
+      <List className="profil-list">
+        <Image
+          src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+          size="medium"
+          circular
         />
-        <p>description card</p>
-        <Button icon='trash alternate' />
+        <div className="profil-description">
+          <List.Item>
+            <List.Icon name="user" />
+            <List.Content>John | Wilkinson  </List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="marker" />
+            <List.Content>New York, NY</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="mail" />
+            <List.Content>
+              <a href="mailto:Yves@travelInc.com">John@travelInc.com</a>
+            </List.Content>
+          </List.Item>
+        </div>
+        <div className="profil-button">
+          <Button>Créer/Modifier</Button>
+          <Button>Changer mot de passe</Button>
+        </div>
+      </List>
+      <div className="favorites">
+        <h2 className="favorites-title">Listes favoris</h2>
+        <List className="favorites-list">
+          <Image
+            src="https://react.semantic-ui.com/images/wireframe/image.png"
+            size="medium"
+            disabled
+          />
+          <p>description card</p>
+          <Button icon="trash alternate" />
+        </List>
       </div>
     </div>
   );
