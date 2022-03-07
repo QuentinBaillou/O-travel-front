7c7cb5e9222e7d817b44430c2ff9b6a56455f08d
QuentinBaillou <quentin.baillou@hotmail.fr>
2022-03-07 10:27:06 +0100

    Fix - Url api

--- src/middlewares/list.js
+++ src/middlewares/list.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
-import axios from 'axios';
 import { GET_DESTINATIONS, saveDestinations } from 'src/actions/listActions';
+import axiosInstance from 'src/axiosInstance';
 
 const fetchList = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_DESTINATIONS:
-      axios
-        .get('http://leluya-server.eddi.cloud/projet-23-o-travel-back/public/api/destinations/list')
+      axiosInstance
+        .get('destinations/list')
         .then((response) => {
           store.dispatch(saveDestinations(response.data));
         })
