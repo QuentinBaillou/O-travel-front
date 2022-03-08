898ccbae0b0909b3f043da42658333f00094961a
QuentinBaillou <quentin.baillou@hotmail.fr>
2022-03-08 11:07:08 +0100

    Rebase

--- src/components/Inscription/index.js
+++ src/components/Inscription/index.js
@@ -1,11 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { setEmail, setPassword, setFirstname, setLastname } from 'src/actions/signin'; 
-import { setSubmitted, setError, setCreateUser } from 'src/actions/signin';
+import {
+  setEmail, setPassword, setFirstname, setLastname,
+  setSubmitted, setError, setCreateUser,
+} from 'src/actions/signin';
 
 import './style.scss';
 
 const Inscription = () => {
-
   const dispatch = useDispatch();
 
   const email = useSelector((state) => state.signin.email);
@@ -32,35 +33,33 @@ const Inscription = () => {
     else {
       dispatch(setSubmitted(true));
       dispatch(setError(false));
-      setCreateUser();
+      dispatch(setCreateUser());
     }
   };
 
   // Si authentification réussie
-  const successMessage = () => {
-    return (
-      <div
-        className="success"
-        style={{
-          display: submitted ? '' : 'none',
-        }}>
-        <h1>L'utilisateur {firstname} {lastname} a bien été enregistré !</h1>
-      </div>
-    );
-  };
- 
+  const successMessage = () => (
+    <div
+      className="success"
+      style={{
+        display: submitted ? '' : 'none',
+      }}
+    >
+      <h1>L'utilisateur {firstname} {lastname} a bien été enregistré !</h1>
+    </div>
+  );
+
   // Si persiste des erreurs
-  const errorMessage = () => {
-    return (
-      <div
-        className="error"
-        style={{
-          display: error ? '' : 'none',
-        }}>
-        <h1>Merci de remplir tous les champs</h1>
-      </div>
-    );
-  };
+  const errorMessage = () => (
+    <div
+      className="error"
+      style={{
+        display: error ? '' : 'none',
+      }}
+    >
+      <h1>Merci de remplir tous les champs</h1>
+    </div>
+  );
 
   const handleEmail = (event) => {
     dispatch(setEmail(event.target.value));
@@ -83,29 +82,29 @@ const Inscription = () => {
   };
 
   return (
-      <div className="signin">
+    <div className="signin">
 
-        <div className="signin_messages">
-          {errorMessage()}
-          {successMessage()}
-        </div>
-
-        <h2 className="signin_title">Créez votre espace perso</h2><br />
-
-        <form onSubmit={handleSubmit} className="signin_form">
-            <label htmlFor="email">E-mail</label><br />
-              <input className="signin_form__input" size="60" value={email} onChange={handleEmail} className="form__input" name="email" type="email" placeholder="Saisissez votre e-mail" /><br />
-            <label htmlFor="password">Mot de passe</label><br />
-              <input className="signin_form__input" size="60" value={password} onChange={handlePassword} className="form__input" name="password" type="password" placeholder="Saisissez votre mot de passe" /><br />
-            <label htmlFor="firstname">Prénom</label><br />
-              <input className="signin_form__input" size="60" value={firstname} onChange={handleFirstname} className="form__input" name="firstname" type="text" placeholder="Saisissez votre prénom" /><br />
-            <label htmlFor="lastname">Nom</label><br />
-              <input className="signin_form__input" size="60" value={lastname} onChange={handleLastname} className="form__input" name="lastname" type="text" placeholder="Saisissez votre nom" /><br /><br />
-
-          <button className="signin_form__submit" type="submit">Sign In</button>
-        </form>
-
+      <div className="signin_messages">
+        {errorMessage()}
+        {successMessage()}
       </div>
+
+      <h2 className="signin_title">Créez votre espace perso</h2><br />
+
+      <form onSubmit={handleSubmit} className="signin_form">
+        <label htmlFor="email">E-mail</label><br />
+        <input className="signin_form__input" size="60" value={email} onChange={handleEmail} className="form__input" name="email" type="email" placeholder="Saisissez votre e-mail" /><br />
+        <label htmlFor="password">Mot de passe</label><br />
+        <input className="signin_form__input" size="60" value={password} onChange={handlePassword} className="form__input" name="password" type="password" placeholder="Saisissez votre mot de passe" /><br />
+        <label htmlFor="firstname">Prénom</label><br />
+        <input className="signin_form__input" size="60" value={firstname} onChange={handleFirstname} className="form__input" name="firstname" type="text" placeholder="Saisissez votre prénom" /><br />
+        <label htmlFor="lastname">Nom</label><br />
+        <input className="signin_form__input" size="60" value={lastname} onChange={handleLastname} className="form__input" name="lastname" type="text" placeholder="Saisissez votre nom" /><br /><br />
+
+        <button className="signin_form__submit" type="submit">Sign In</button>
+      </form>
+
+    </div>
   );
 };
 
