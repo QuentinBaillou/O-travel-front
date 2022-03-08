0176792ee63d45610440c96f2c5ed9676f0d3c09
QuentinBaillou <quentin.baillou@hotmail.fr>
2022-03-08 11:47:33 +0100

    Format - Eslint

--- src/middlewares/signin.js
+++ src/middlewares/signin.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-console */
 import axiosInstance from 'src/axiosInstance';
 import { CREATE_USER } from 'src/actions/signin';
 import { login } from 'src/actions/authenticationActions';
 
 const signInMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
-    case CREATE_USER:
+    case CREATE_USER: {
       const state = store.getState();
       axiosInstance
         .post('user/form', {
@@ -14,17 +15,18 @@ const signInMiddleware = (store) => (next) => (action) => {
           password: state.signin.password,
         })
         .then((response) => {
+          console.log(response);
           store.dispatch(login());
         })
         .catch((error) => {
-          (console.log(error))
+          (console.log(error));
         });
       next(action);
-      break;
+      break; }
     default:
       next(action);
       break;
-  };
+  }
 };
 
 export default signInMiddleware;
