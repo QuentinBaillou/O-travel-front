520b433ebf695645878d9e3ebe8140de9a121da3
Rudylefrancois <rudylefrancois18@gmail.com>
2022-03-15 15:05:26 +0100

    func : add link for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,12 @@
 import './style.scss';
-
+import { Link } from 'react-router-dom';
 import { List } from 'semantic-ui-react';
 
 const Footer = () => (
   <div className="footer">
     <List horizontal>
-      <List.Item href="/mentions-legales">Mentions légales</List.Item>
-      <List.Item href="/contact">Contactez-nous</List.Item>
+      <List.Item><Link to="/mentions-legales">Mentions légales</Link></List.Item>
+      <List.Item><Link to="/contact">Contactez-nous</Link></List.Item>
     </List>
     <List>
       <List.Item>© O'Travel 2022, Inc.</List.Item>
